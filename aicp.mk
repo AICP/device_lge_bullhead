46332ad610cd1998bc9ca695d3c2d42998b4741b
doc HD <doc.divxm@gmail.com>
2017-10-03 13:08:36 +0300

    bullhead: AICP fy
    Change-Id: Iae0ddfe0c67688f063803367a941e6c0423729ee

---
+++ aicp.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aicp_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM1.171019.011/4448085:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="doc HD (semdoc)"
